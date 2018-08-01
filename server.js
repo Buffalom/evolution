9dc8e660fa5812ac663aa2ee568dbe9e2479d4d7
Tom Diggelmann <mail@tomdgm.ch>
2018-08-01 15:47:54 +0200

    Added node server to host app and save/get worldHashes
    - Reset app to begin newly

--- server.js
+++ server.js
@@ -1,8 +1,60 @@
+const fs = require('fs')
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-var port = process.env.PORT || 5000
-var server = app.listen(port)
-console.log(`Server listening on port ${port}`)
+
+function makeId (length) {
+  if (!length) throw new Error('Please provide a length to generate an id')
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var idArray = new Array(length).fill(undefined)
+  return idArray.map(() => chars.charAt(Math.floor(Math.random() * chars.length))).join('')
+}
+
+function initFileIfNotExists (path) {
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify({
+      worlds: {}
+    }, null, '  '))
+  }
+}
+
+
+const idLength = 5
+const serverDataFile = './serverData.json'
+
+app.get('/world/:id([a-zA-Z0-9]{5})', (req, res) => {
+  initFileIfNotExists(serverDataFile)
+  let data = JSON.parse(fs.readFileSync(serverDataFile))
+  data.worlds[req.params.id]
+    ? res.send(data.worlds[req.params.id])
+    : res.status(404).send({ message: `No world with id ${req.params.id} found.`})
+})
+
+app.post('/world', (req, res) => {
+  initFileIfNotExists(serverDataFile)
+  let data = JSON.parse(fs.readFileSync(serverDataFile))
+  let maxCycles = 10
+  for (let cycle = 0; cycle < maxCycles; cycle++) {
+    let id = makeId(idLength)
+    if (!data.worlds[id]) {
+      data.worlds[id] = {
+        worldHash: req.query.worldHash || ''
+      }
+      fs.writeFileSync(serverDataFile, JSON.stringify(data, null, '  '))
+      res.send({ id })
+      return
+    }
+  }
+  res.status(500).send({ message: `No available id could be generated with ${maxCycles} tries.`})
+})
+
+app.get('/', express.static(__dirname + '/public'))
+app.get('/assets/*', express.static(__dirname + '/public'))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`)
+})
